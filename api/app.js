b9eeca3df5667851c313d7809e37fa4f0093a644
Tony <extracat@users.noreply.github.com>
2023-10-01 14:09:40 +0200

    save

--- api/app.js
+++ api/app.js
@@ -3,8 +3,8 @@ const app = express();
 
 // Application-level middleware, routes, etc.
 
-const apiV1Routes = require('./routes/api-v1');
-const apiV2Routes = require('./routes/api-v2');
+const apiV1Routes = require('../routes/api-v1');
+const apiV2Routes = require('../routes/api-v2');
 
 app.use('/api/v1', apiV1Routes);
 app.use('/api/v2', apiV2Routes);
