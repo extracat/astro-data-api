fa30aba2967e4702febc28c035cae12e2b8fb6e6
Tony <extracat@users.noreply.github.com>
2024-04-25 23:18:35 +0200

    save

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 // Swagger UI page
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
