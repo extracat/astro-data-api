e15df020fa1d45e699befa89683b6b7f05635f3c
Tony <extracat@users.noreply.github.com>
2024-04-25 21:03:40 +0200

    save

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../index.json');
+
+// Swagger UI page
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
+
+module.exports = app;
