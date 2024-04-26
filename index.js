b004d094d04f08682fc32bd695f43e0b80b0fde7
Tony@WorkBook <extracat@users.noreply.github.com>
2024-04-26 15:44:14 +0200

    swagger test 100500

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const app = require('./api/index');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./index.json');
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 // Swagger UI page
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
